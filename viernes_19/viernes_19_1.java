4d897123ec24c42a161844eff3472fbebc263b16
Carlos Pastor <carlos960617@gmail.com>
2021-11-17 12:39:54 +0100

    Viernes 19 1

---
+++ viernes_19/viernes_19_1.java
@@ -0,0 +1,46 @@
+package com.company.viernes_19;
+
+import java.util.Scanner;
+
+public class viernes_19_1 {
+
+    public static boolean esImpar(int numero){
+
+        return numero > 0 && numero%2!= 0;
+    }
+
+    public static int sumarImpares(int inicio, int fin){
+
+        int suma = 0;
+
+        if(inicio > 0 && fin >= inicio){
+
+            for(int i=inicio; i<=fin;i++){
+                if(esImpar(i)){
+                    suma += i;
+                }
+            }
+
+        }else{suma = -1;}
+
+        return suma;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce dos numeros para averiguar el sumatorio de los primos comprendidos entre estos (ambos incluidos)");
+
+        int init = sc.nextInt();
+
+        int end = sc.nextInt();
+
+        int result = sumarImpares(init, end);
+
+        System.out.println("El resultado es: "+result);
+
+
+
+    }
+}
