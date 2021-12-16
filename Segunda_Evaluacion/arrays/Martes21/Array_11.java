f3c34bb64ce30359c533fcf043340d026e87e1ad
Carlos Pastor <carlos960617>
2021-12-16 20:30:19 +0100

    Ej 11

--- Segunda_Evaluacion/arrays/Martes21/Array_11.java
+++ Segunda_Evaluacion/arrays/Martes21/Array_11.java
@@ -3,20 +3,15 @@ package com.company.Segunda_Evaluacion.arrays.Martes21;
 public class Array_11 {
 
     public static boolean isArrayOn (int[] src, int[] dst) {
-
-        int shortI = 0;
         int coincide = 0;
 
         for(int i = 0;i<dst.length;i++){
-            if(shortI > src.length-1){
-                shortI =0;
-            }
+
             for(int j = 0;j<src.length;j++){
-                if(dst[i] == src[shortI]){
+                if(dst[i] == src[j]){
                     coincide++;
                 }
             }
-         shortI++;
         }
 
         if(coincide >= src.length){
@@ -26,9 +21,9 @@ public class Array_11 {
 
     public static void main(String[] args) {
 
-        int[] array = {3, 5, -5, 6, 27, 44, 69, -100, 1, 0};
+        int[] array = {3, 5, -5, 6, 27, 44, 69, -100, 1, 0, 2000};
 
-        int[] array2 = {44, 69, -100};
+        int[] array2 = {2000, 69, -100};
 
         System.out.println(isArrayOn(array2, array));
 
